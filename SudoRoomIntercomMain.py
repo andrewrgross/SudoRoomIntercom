2722a39e1e73614f5ad4a11f27803ca44526a865
Andrew R Gross <arg5029@gmail.com>
2022-10-22 16:37:54 -0700

    Major updates

--- SudoRoomIntercomMain.py
+++ SudoRoomIntercomMain.py
@@ -10,29 +10,42 @@ A constantly running program that turns on a kiosk-style looping slideshow in th
 ### - 0 - Libraries
 import pygame
 import time
-import cv2
+#import cv2
 import RPi.GPIO as GPIO
 #import glob
 #import numpy as np
 #import io,sys,os,subprocess
 #global process
 
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import selenium
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 
 ##############################################################################
 ### - 1 - Functions
 
 
-
 ##############################################################################
 ### - 2 - Variables
+
+slideshowURL = 'https://docs.google.com/presentation/d/e/2PACX-1vRsIRyslE2K0WIiMawiZuHpU2KHexuSipMXPQTy4ABqsh5MjzRhRz2C5WwdRni3bB_1Ial2Dm0mObrL/pub?start=true&loop=true&delayms=2000'
+sudoroomURL = 'http://meet.waag.org/turtlesturtlesturtles'
+
+chromedriver_location = "/home/andrew/Programs/chromedriver"
+driver = webdriver.Chrome(chromedriver_location)
 
 ##################################
 ## - 2.1 - Assets
 ## Images
-menucontrol = pygame.image.load('/home/pi/menu-control.png')
+menucontrol = pygame.image.load('./Assets/menu-control.png')
 
 ## Sounds
-beep1 = pygame.mixer.Sound('/home/pi/beep.wav')
+beep1 = pygame.mixer.Sound('./Assets/beep1.wav')
+
 
 ##################################
 ## - 2.1 - Pins
@@ -53,6 +66,12 @@ GPIO.setup(button3,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 ### - 3 - Startup commands
 
 ### Open browser to slideshow
+#driver.get(slideshowURL)
+webbrowser.open(slideshowURL)
+webbrowser.open(sudoroomURL)
+
+driver.get(slideshowURL)
+driver.get(sudoroomURL)
 
 ##############################################################################
 ### - 4 - Main loop
