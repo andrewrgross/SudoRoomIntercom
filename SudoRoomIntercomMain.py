f2cdec2a0f8fc756957cc795532c8f9568e50cd0
Andrew R Gross <arg5029@gmail.com>
2022-10-23 22:08:07 -0700

    added full screen

--- SudoRoomIntercomMain.py
+++ SudoRoomIntercomMain.py
@@ -15,13 +15,21 @@ from time import sleep
 #import io,sys,os,subprocess
 #global process
 
+import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import selenium
+
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.options import Options    
+
+chrome_options = Options()
+chrome_options.add_experimental_option("useAutomationExtension", False)
+chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+chrome_options.add_argument("--start-fullscreen");
+#chrome_options.add_argument("--kiosk");
 
 ##############################################################################
 ### - 1 - Functions
@@ -30,11 +38,10 @@ from selenium.webdriver.common.action_chains import ActionChains
 ##############################################################################
 ### - 2 - Variables
 
-slideshowURL = 'https://docs.google.com/presentation/d/e/2PACX-1vRsIRyslE2K0WIiMawiZuHpU2KHexuSipMXPQTy4ABqsh5MjzRhRz2C5WwdRni3bB_1Ial2Dm0mObrL/pub?start=true&loop=true&delayms=2000'
+slideshowURL = 'https://docs.google.com/presentation/d/e/2PACX-1vRsIRyslE2K0WIiMawiZuHpU2KHexuSipMXPQTy4ABqsh5MjzRhRz2C5WwdRni3bB_1Ial2Dm0mObrL/pub?start=true&loop=true&delayms=5000'
 sudoroomURL = 'http://meet.waag.org/turtlesturtlesturtles'
 
-#chromedriver_location = "/home/andrew/Programs/chromedriver"
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(chrome_options=chrome_options)
 
 ##################################
 ## - 2.1 - Assets
@@ -60,6 +67,11 @@ GPIO.setup(button1,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(button2,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(button3,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+##################################
+## - 2.1 - States
+
+state = 0   # 0: slideshow; 1: Menu;  2: Sudo Room intercom
+
 ##############################################################################
 ### - 3 - Startup commands
 
@@ -77,21 +89,45 @@ driver.get(slideshowURL)
 
 print('Beginning loop')
 
+"""
+
+pygame.init()
+screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+screen.fill((255, 255, 255))
+# Draw a solid blue circle in the center
+pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+
+font = pygame.font.SysFont(None, 66)
+font2 = pygame.font.SysFont(None, 50)
+
+
+screen.blit(menucontrol, (0,0))
+pygame.display.update()
+
+
+"""
+
 ##############################################################################
 ### - 4 - Main loop
 
 while True:
-    if GPIO.input(button1) == False:
-        # Display loading message
-        driver.get(sudoroomURL)
-        # Clear loading message
-        # Agree to provide mic and camera support
+    if state == 0:      # Slideshow mode
+            if GPIO.input(button1) == False:
+                # Display loading message
+                driver.get(sudoroomURL)
+                # Clear loading message
+                # Agree to provide mic and camera support
+                
+                sleep(15)
+                #timerVal = 15
+                driver.get(slideshowURL)
+                
+            if GPIO.input(button2) == False:
+                break
+                #Make this go to
 
-        sleep(15)
-        driver.get(slideshowURL)
-
-    if GPIO.input(button2) == False:
-        break
+    if state == 1:
+        pass
     
 # Close browser window
 print('Done')
